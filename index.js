e2e0540709437d778e905f1683d154f6e98ae998
1chi <Nyes3704@gmail.com>
2020-12-17 03:21:04 +0000

    added more assets

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.set('json spaces', 4);
 
-app.use(favicon(path.join(__dirname, 'public',  'img', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
 app.use('/img', express.static(path.join(__dirname, 'public', 'img')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,11 +19,13 @@ app.use(helmet());
 morganBody(app);
 
 app.get('/', (req, res) => {
-	res.status(200).send('Hello, World! oWo uWu');
+        res.status(200).send('Hello, World! oWo uWu');
 });
 
 app.listen(3000, () => {
-	console.log(`Server started at ${new Date().toDateString()} and at port 3000.`);
+        console.log(
+                `Server started at ${new Date().toDateString()} and at port 3000.`
+        );
 });
 
 setInterval(() => fetch('https://website.1chi.repl.co'), 360000);
